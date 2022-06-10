c51573b5be65aaa5872a46c4fbd30b8991eb1eeb
katwojuek <wojcikk@uek.krakow.pl>
2022-06-10 11:15:18 +0200

    2022-06-10 KW

--- app/routes.py
+++ app/routes.py
@@ -7,29 +7,11 @@ import os
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+from app.models.product import Product
 
-def get_item(ancestor, selector, attribute=None, return_list=False):
-    try:
-        if return_list:
-            return [item.get_text().strip() for item in ancestor.select(selector)]
-        if attribute:
-            return ancestor.select_one(selector)[attribute]
-        return ancestor.select_one(selector).get_text().strip()
-    except (AttributeError, TypeError):
-        return None
 
-selectors = {
-    "author": ["span.user-post__author-name"],
-    "recommendation": ["span.user-post__author-recomendation > em"],
-    "stars": ["span.user-post__score-count"],
-    "content": ["div.user-post__text"],
-    "useful": ["button.vote-yes > span"],
-    "useless": ["button.vote-no > span"],
-    "publish_date": ["span.user-post__published > time:nth-child(1)", "datetime"],
-    "purchase_date": ["span.user-post__published > time:nth-child(2)", "datetime"],
-    "pros": ["div[class$=positives]~ div.review-feature__item", None, True],
-    "cons": ["div[class$=negatives]~ div.review-feature__item", None, True]
-}
+
+
 
 @app.route('/')
 def index():
@@ -39,27 +21,10 @@ def index():
 def extract():
     if request.method == "POST":
         product_id = request.form.get("product_id")
-        url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
-        all_opinions = []
-        while(url):
-            response = requests.get(url)
-            page = BeautifulSoup(response.text, 'html.parser')
-            opinions = page.select("div.js_product-review")
-            for opinion in opinions:
-                single_opinion = {
-                    key:get_item(opinion, *value)
-                        for key, value in selectors.items()
-                }
-                single_opinion["opinion_id"] = opinion["data-entry-id"]
-                all_opinions.append(single_opinion)
-            try:    
-                url = "https://www.ceneo.pl"+get_item(page,"a.pagination__next","href")
-            except TypeError:
-                url = None
-            if not os.path.exists("app/opinions"):
-                os.makedirs("app/opinions")
-            with open(f"app/opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
-                json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
+        product = Product(product_id)
+        product.extract_product()
+        
+        
         return redirect(url_for("product", product_id=product_id))
     else:
         return render_template("extract.html.jinja")
@@ -77,12 +42,7 @@ def author():
 def product(product_id):
     opinions = pd.read_json(f"app/opinions/{product_id}.json")
     opinions.stars = opinions.stars.map(lambda x: float(x.split("/")[0].replace(",", ".")))
-    stats = {
-        "opinions_count": len(opinions.index),
-        "pros_count": opinions.pros.map(bool).sum(),
-        "cons_count": opinions.cons.map(bool).sum(),
-        "average_score": opinions.stars.mean().round(2)
-    }
+    
     recommendation = opinions.recommendation.value_counts(dropna = False).sort_index().reindex(["Nie polecam", "Polecam", None])
     recommendation.plot.pie(
         label="", 
